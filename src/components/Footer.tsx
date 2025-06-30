4025fd0da9dc21aa7ad1815887c843cdab8421d0
CADU54373 <carloseduardi2311@gmail.com>
2025-06-30 20:27:30 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -94,7 +94,7 @@ const Footer = () => {
         
         <div className="mt-8 pt-8 border-t border-gray-800/50 text-center">
           <p className="text-gray-500">
-            © 2024 Carlos Eduardo. Todos os direitos reservados. Feito com ❤️ e muito ☕
+            © 2024 Carlos Eduardo. Todos os direitos reservados.  e muito ☕
           </p>
         </div>
       </div>
